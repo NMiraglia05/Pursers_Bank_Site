64c0c3c0c0dd8b4f9aa64865b20c9546d0281f55
NMiraglia05 <nmiraglia05@gmail.com>
2025-10-21 09:15:22 -0700

    Update Sandbox.py

--- Sandbox.py
+++ Sandbox.py
@@ -1,1 +1,117 @@
+import uuid
+from datetime import date
 
+class Transaction(ABC):
+    @property
+    @abstractmethod
+    def ID(self):
+        """Unique identifier for the transaction."""
+        pass
+
+    @abstractmethod
+    def save(self):
+        """Save transaction details."""
+        pass
+
+class Account(ABC):
+    @abstractmethod
+    def update(self):
+        """Update the account."""
+        pass
+
+    @abstractmethod
+    def create(self):
+        """create the account."""
+        pass
+
+    @abstractmethod
+    def save(self):
+        """Save the account to the db."""
+        pass
+
+class Payment(Transaction):
+    def __init__(self):
+        pass
+
+    def save(self):
+        if self.value is None:
+            raise InvalidPaymentDetails
+        trans={'id':self.id,'type':self.type,'desc':self.desc,'value':self.value}
+        return trans
+
+class Credit(Payment):
+    def __init__(self,amount):
+        self.type='Credit'
+        self.value=amount*-1 # because this is a credit transaction, this bills at a negative. The assumption being an account only pays with credit to cover a gap created by insufficient funds.
+        self.desc='Future payment for goods/services received'
+
+class Cash(Payment):
+    def __init__(self,amount):
+        self.type='Cash'
+        self.value=amount
+        self.desc='Direct Currency'
+
+class Materials(Payment):
+    def __init__(self,materials):
+        self.type='Materials'
+        self.value=0
+        for mat,amount in materials.items():
+            mat_value=item_lookup[mat]['points']
+            trans_value=mat_value*amount
+            self.value+=trans_value
+        self.desc=materials
+
+class Service(Payment):
+    def __init__(self,value=None):
+        self.type='Services'
+        self.value=value # this is so that an adjustor can review the service provided and determine the value thereof. If a service payment is attempted without a value being set, it will not work.
+        self.desc='Services provided for goods/services received.'
+
+class Person(Account):
+    def __init__(self):
+        self.character_name=details.character_name
+        self.player_name=details.player_name
+        self.discord=details.discord
+    
+    def create(self):
+        self.uuid=str(uuid.uuid4())
+        self.id=max(table)+1
+
+class Employee(Person):
+    def __init__(self,details):
+        super().__init__()
+
+    def create(self,cat='employee'):
+        super().create(cat)
+        self.character_name=details.character
+        self.player_name=details.player
+        self.rank=details.rank
+        self.day_created=date.today()
+
+class Customer(Person):
+    def __init__(self):
+        def __super__():
+            pass
+        self.log_message=f'Customer {self.character_name}'
+        self.accounts=[]
+
+class Savings_Account(Account):
+    def __init__(self):
+        self.id=none
+        self.customer_id=none
+
+    def Update_Balance(self):
+        pass
+
+class Order(Transaction):
+    def __init__(self,order,customer):
+        self.id=None
+        self.Recipient=customer.character_name
+        self.discord_id=customer.discord
+        self.receipient_id=order.customer_id
+        self.items=[]
+        for item,amount in orders.items():
+            for _ in range(amount):
+                self.items.append(item)
+
+class Process_Manager:
